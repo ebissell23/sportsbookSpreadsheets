eabe6802ac50266108410272392ba12ac8334138
Erik Bissell <erikbissell@MacBook-Pro-7.tbissell.net>
2023-02-16 02:30:01 -0500

    making progress. Bet time

--- draftkings.py
+++ draftkings.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
+
 #gets path from driverpath.txt
 #keeps things private
 with open('driverpath.txt','r') as f:
     driver_path = f.read()
     f.close()
-
 
 # Start the ChromeDriver service
 service = Service(driver_path)
@@ -16,7 +16,21 @@ service.start()
 driver = webdriver.Chrome(service=service)
 
 # Navigate to a web page
-driver.get('https://www.example.com')
+driver.get('https://oddsjam.com/bet-tracker')
+print('before')
+with open('loginCookie.txt','r') as f:
+    loginCookieDict = {}
+    for line in f:
+        key, value = line.strip().split('=')
+        loginCookieDict[key] = value
+    loginCookie = f.read()
+    f.close()
 
+driver.add_cookie(loginCookieDict)
+driver.refresh()
+#element = driver.find_element_by_class_name('dkcss-1l4mw41')
+#print(element)
+print('after')
 # Close the browser
+input()
 driver.quit()
